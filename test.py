8e0bc568e9b64c99dca05c732f0be5d4bdc37e14
Masay03 <maks.bz15@mail.ru>
2024-05-06 15:21:24 +0700

    new code

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 # Функция для проверки существования файла
+import os
+
 def exists(path):
     """Проверяет существует ли файл по указанному пути"""
     return os.path.exists(path) and os.path.isfile(path)
