f682f52864f35cc5dcf6f6885f394d97c0d48b95
Masay03 <maks.bz15@mail.ru>
2024-05-06 15:32:25 +0700

    new code

---
+++ main.py
@@ -0,0 +1,5 @@
+file_path = "C:\Users\maksb\PycharmProjects\pythonProject\text.py"
+if exists(file_path):
+    print("Файл существует")
+else:
+    print("Файл не найден")
--- test.py
+++
@@ -1,4 +0,0 @@
-# Функция для проверки существования файла
-def exists(path):
-    """Проверяет существует ли файл по указанному пути"""
-    return os.path.exists(path) and os.path.isfile(path)
