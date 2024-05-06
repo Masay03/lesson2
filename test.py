362cc8d07ae42592624a696e9a0221345ddd996f
Masay03 <maks.bz15@mail.ru>
2024-05-06 13:59:38 +0700

    new code

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-print("Hello World")
-print("Hello World")
-print("Hello World")
-print("Hello World")
-print("Hello World")                
+# Функция для проверки существования файла
+def exists(path):
+    """Проверяет существует ли файл по указанному пути"""
+    return os.path.exists(path) and os.path.isfile(path)
