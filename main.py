b60dd00f26a187b91d32e5b74effdec9df5ca4ed
Masay03 <maks.bz15@mail.ru>
2024-05-08 17:17:03 +0700

    new code

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+<<<<<<< Updated upstream
 file_path = "C:\Users\maksb\PycharmProjects\pythonProject\main.py"
 if exists(file_path):
     print("Файл существует")
 else:
     print("Файл не найден")
+=======
+>>>>>>> Stashed changes
